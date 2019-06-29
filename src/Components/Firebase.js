6878f54505dc2d0efa02f2e8f09cccc2a2504f45
unknown <j.dgez.p@gmail.com>
2019-06-28 20:39:58 -0500

    maquetación del componente navbar

---
+++ src/Components/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyC8tF0MvIHH9yV60lkZ8dULoDVS0sWQ1gs",
+    authDomain: "arkano-test-7fad5.firebaseapp.com",
+    databaseURL: "https://arkano-test-7fad5.firebaseio.com",
+    projectId: "arkano-test-7fad5",
+    storageBucket: "",
+    messagingSenderId: "734194396122",
+    appId: "1:734194396122:web:6996f286bd3a1355"
+  };
+
+  const firebaseConf = firebase.initializeApp(config);
+export default firebaseConf;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <span className="eVentas">
+                <h2> e-Ventas </h2>
+            </span>
+        </header>
+    )
+}
+
+export default Header;
