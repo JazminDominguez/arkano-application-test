aad28196a96437e306d46ef8e0c0a0e06f1c5736
unknown <j.dgez.p@gmail.com>
2019-07-01 09:20:32 -0500

    Updates

---
+++ precache-manifest.af2daf5901a573382615a6c28998f026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a53d78d95842bac4320c13cd3e43c3",
+    "url": "/arkano-application-test/index.html"
+  },
+  {
+    "revision": "3dac114339628f718858",
+    "url": "/arkano-application-test/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "517181e5bfb8be8d7329",
+    "url": "/arkano-application-test/static/css/main.d1bbc25a.chunk.css"
+  },
+  {
+    "revision": "3dac114339628f718858",
+    "url": "/arkano-application-test/static/js/2.86279c7e.chunk.js"
+  },
+  {
+    "revision": "517181e5bfb8be8d7329",
+    "url": "/arkano-application-test/static/js/main.443373cb.chunk.js"
+  },
+  {
+    "revision": "00ccf46fa56bb4d54b30",
+    "url": "/arkano-application-test/static/js/runtime~main.75ef8549.js"
+  }
+]);
